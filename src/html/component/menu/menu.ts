8467a102cfa990ad2f3be949940ed4beb3f5ac60
Petr Ivičič <492712@mail.muni.cz>
2023-11-02 18:10:42 +0100

    added dummy hamburger menu

---
+++ src/html/component/menu/menu.ts
@@ -0,0 +1,23 @@
+class Menu extends HTMLElement {
+    shadow;
+
+    constructor() {
+        super();
+        const template = document.getElementById('menu')!;
+        // @ts-ignore
+        const content = template.content;
+        this.shadow = this.attachShadow({mode: 'open'});
+        this.shadow.appendChild(content.cloneNode(true));
+        const linkElem = document.createElement('link');
+        linkElem.setAttribute('rel', 'stylesheet');
+        linkElem.setAttribute('href', 'component/menu/menu.less');
+        this.shadow.appendChild(linkElem);
+    }
+
+    connectedCallback() {
+
+    }
+
+}
+
+customElements.define('hamburger-menu', Menu);
