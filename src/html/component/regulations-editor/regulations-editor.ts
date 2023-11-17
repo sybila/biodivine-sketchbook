75117b55318db9096e8faa9733ecdf5983e31b3f
Petr Ivičič <492712@mail.muni.cz>
2023-11-17 16:38:34 +0100

    added graph state saving

--- src/html/component/regulations-editor/regulations-editor.ts
+++ src/html/component/regulations-editor/regulations-editor.ts
@@ -15,6 +15,9 @@ import './node-menu'
 import { edgeOptions, initOptions } from './regulations-editor.config'
 import { ElementType, Monotonicity } from './element-type'
 
+const SAVE_NODES = 'nodes'
+const SAVE_EDGES = 'edges'
+
 @customElement('regulations-editor')
 class RegulationsEditor extends LitElement {
   static styles = css`${unsafeCSS(style_less)}`
@@ -53,7 +56,9 @@ class RegulationsEditor extends LitElement {
     // this._nodes.push({ data: { id: 'test', label: 'test' } })
     this.cy = cytoscape(initOptions(this.editorElement, this._nodes, this._edges))
     this.edgeHandles = this.cy.edgehandles(edgeOptions)
-    this.dummyData()
+    if (!this.loadCachedNodes() || !this.loadCachedEdges()) this.loadDummyData()
+
+    // this.cy.on('add remove position', this.saveState)
 
     this.cy.on('zoom', () => {
       this._renderMenuForSelectedNode()
@@ -78,39 +83,19 @@ class RegulationsEditor extends LitElement {
     })
   }
 
-  dummyData (): void {
-    this.addNode('YOX1', 'YOX1', [297, 175])
-    this.addNode('CLN3', 'CLN3', [128, 68])
-    this.addNode('YHP1', 'YHP1', [286, 254])
-    this.addNode('ACE2', 'ACE2', [74, 276])
-    this.addNode('SWI5', 'SWI5', [47, 207])
-    this.addNode('MBF', 'MBF', [219, 96])
-    this.addNode('SBF', 'SBF', [281, 138])
-    this.addNode('HCM1', 'HCM1', [305, 217])
-    this.addNode('SFF', 'SFF', [186, 302])
-    this.ensureRegulation({ source: 'MBF', target: 'YOX1', observable: true, monotonicity: Monotonicity.ACTIVATION })
-    this.ensureRegulation({ source: 'SBF', target: 'YOX1', observable: true, monotonicity: Monotonicity.ACTIVATION })
-    this.ensureRegulation({ source: 'YOX1', target: 'CLN3', observable: true, monotonicity: Monotonicity.INHIBITION })
-    this.ensureRegulation({ source: 'YHP1', target: 'CLN3', observable: true, monotonicity: Monotonicity.INHIBITION })
-    this.ensureRegulation({ source: 'ACE2', target: 'CLN3', observable: true, monotonicity: Monotonicity.ACTIVATION })
-    this.ensureRegulation({ source: 'SWI5', target: 'CLN3', observable: true, monotonicity: Monotonicity.ACTIVATION })
-    this.ensureRegulation({ source: 'MBF', target: 'YHP1', observable: true, monotonicity: Monotonicity.ACTIVATION })
-    this.ensureRegulation({ source: 'SBF', target: 'YHP1', observable: true, monotonicity: Monotonicity.ACTIVATION })
-    this.ensureRegulation({ source: 'SFF', target: 'ACE2', observable: true, monotonicity: Monotonicity.ACTIVATION })
-    this.ensureRegulation({ source: 'SFF', target: 'SWI5', observable: true, monotonicity: Monotonicity.ACTIVATION })
-    this.ensureRegulation({ source: 'CLN3', target: 'MBF', observable: true, monotonicity: Monotonicity.ACTIVATION })
-    this.ensureRegulation({ source: 'MBF', target: 'SBF', observable: true, monotonicity: Monotonicity.ACTIVATION })
-    this.ensureRegulation({ source: 'YOX1', target: 'SBF', observable: true, monotonicity: Monotonicity.INHIBITION })
-    this.ensureRegulation({ source: 'YHP1', target: 'SBF', observable: true, monotonicity: Monotonicity.INHIBITION })
-    this.ensureRegulation({ source: 'CLN3', target: 'SBF', observable: true, monotonicity: Monotonicity.ACTIVATION })
-    this.ensureRegulation({ source: 'MBF', target: 'HCM1', observable: true, monotonicity: Monotonicity.ACTIVATION })
-    this.ensureRegulation({ source: 'SBF', target: 'HCM1', observable: true, monotonicity: Monotonicity.ACTIVATION })
-    this.ensureRegulation({ source: 'SBF', target: 'SFF', observable: true, monotonicity: Monotonicity.ACTIVATION })
-    this.ensureRegulation({ source: 'HCM1', target: 'SFF', observable: true, monotonicity: Monotonicity.ACTIVATION })
+  loadDummyData (): void {
+    console.log('loading dummy data')
+    this.cy?.nodes().remove()
+    this.cy?.edges().remove()
+    this.addNodes(dummyData.nodes)
+    this.addEdges(dummyData.edges)
+    this.saveState()
   }
 
   render (): TemplateResult {
     return html`
+        <button @click=${this.loadDummyData} class="uk-button uk-button-danger uk-button-small uk-margin-large-left uk-position-absolute uk-position-z-index-high">reset</button>
+
         ${this.editorElement}
         <node-menu .type=${this.menuType} .position=${this.menuPosition} .zoom=${this.menuZoom} .data=${this.menuData}></node-menu>
     `
@@ -149,10 +134,10 @@ class RegulationsEditor extends LitElement {
     this.toggleMenu(ElementType.EDGE, position, zoom, edge.data())
   }
 
-  addNode (id: string, name: string, position = [0, 0]): void {
+  addNode (id: string, name: string, position = { x: 0, y: 0 }): void {
     const node = this.cy?.add({
       data: { id, name },
-      position: { x: position[0], y: position[1] }
+      position
     })
     node?.on('mouseover', () => {
       node.addClass('hover')
@@ -191,11 +176,7 @@ class RegulationsEditor extends LitElement {
     this.menuPosition = position ?? { x: 0, y: 0 }
     this.menuZoom = zoom
     this.menuData = data
-    console.log(data)
-    // if (data !== undefined) {
-    //   element.observabilityButton.updateState(data)
-    //   element.monotonicityButton.updateState(data)
-    // }
+    this.saveState()
   }
 
   ensureRegulation (regulation: IRegulation): void {
@@ -269,6 +250,139 @@ class RegulationsEditor extends LitElement {
     this.cy?.$id(e.detail.id).remove()
     this.toggleMenu(ElementType.NONE)
   }
+
+  saveState (): void {
+    const nodes = ((this.cy?.nodes()) ?? []).map((node): INodeData => {
+      return {
+        id: node.data().id,
+        name: node.data().name,
+        position: node.position()
+      }
+    })
+    const edges: IEdgeData[] = ((this.cy?.edges()) ?? []).map((edge): IEdgeData => {
+      return {
+        id: edge.id(),
+        source: edge.source().id(),
+        target: edge.target().id(),
+        observable: edge.data().observable as boolean,
+        monotonicity: edge.data().monotonicity as Monotonicity
+      }
+    })
+    console.log(nodes)
+    if (nodes.length > 0) localStorage.setItem(SAVE_NODES, JSON.stringify(nodes))
+    if (edges.length > 0) localStorage.setItem(SAVE_EDGES, JSON.stringify(edges))
+    // console.log(JSON.stringify(nodes))
+    console.log(localStorage.getItem(SAVE_EDGES))
+    // console.log(edges)
+  }
+
+  loadCachedNodes (): boolean {
+    try {
+      const parsed = (JSON.parse(localStorage.getItem(SAVE_NODES) ?? '[]') as INodeData[])
+      if (parsed.length === 0) return false
+      this.addNodes(parsed)
+    } catch (e) {
+      return false
+    }
+    console.log('nodes loaded')
+    return true
+  }
+
+  loadCachedEdges (): boolean {
+    try {
+      const parsed = (JSON.parse(localStorage.getItem(SAVE_EDGES) ?? '[]') as IEdgeData[])
+      if (parsed.length === 0) return false
+      this.addEdges(parsed)
+    } catch (e) {
+      return false
+    }
+    console.log('edges loaded')
+    return true
+  }
+
+  addNodes (nodes: INodeData[]): void {
+    nodes.forEach((node) => {
+      this.addNode(node.id, node.name, node.position)
+    })
+  }
+
+  addEdges (edges: IEdgeData[]): void {
+    edges.forEach((edge) => {
+      this.ensureRegulation(edge)
+    })
+  }
+}
+
+const dummyData: { nodes: INodeData[], edges: IEdgeData[] } = {
+  nodes: [
+    {
+      id: 'YOX1',
+      name: 'YOX1',
+      position: { x: 297, y: 175 }
+    },
+    {
+      id: 'CLN3',
+      name: 'CLN3',
+      position: { x: 128, y: 68 }
+    },
+    {
+      id: 'YHP1',
+      name: 'YHP1',
+      position: { x: 286, y: 254 }
+    },
+    {
+      id: 'ACE2',
+      name: 'ACE2',
+      position: { x: 74, y: 276 }
+    },
+    {
+      id: 'SWI5',
+      name: 'SWI5',
+      position: { x: 47, y: 207 }
+    },
+    {
+      id: 'MBF',
+      name: 'MBF',
+      position: { x: 219, y: 96 }
+    },
+    {
+      id: 'SBF',
+      name: 'SBF',
+      position: { x: 281, y: 138 }
+    },
+    {
+      id: 'HCM1',
+      name: 'HCM1',
+      position: { x: 305, y: 217 }
+    },
+    {
+      id: 'SFF',
+      name: 'SFF',
+      position: { x: 186, y: 302 }
+    }
+  ],
+  edges: [
+    { source: 'MBF', target: 'YOX1', observable: true, monotonicity: Monotonicity.ACTIVATION, id: '' },
+    { source: 'SBF', target: 'YOX1', observable: true, monotonicity: Monotonicity.ACTIVATION, id: '' },
+    { source: 'YOX1', target: 'CLN3', observable: true, monotonicity: Monotonicity.INHIBITION, id: '' },
+    { source: 'YHP1', target: 'CLN3', observable: true, monotonicity: Monotonicity.INHIBITION, id: '' },
+    { source: 'ACE2', target: 'CLN3', observable: true, monotonicity: Monotonicity.ACTIVATION, id: '' },
+    { source: 'SWI5', target: 'CLN3', observable: true, monotonicity: Monotonicity.ACTIVATION, id: '' },
+    { source: 'MBF', target: 'YHP1', observable: true, monotonicity: Monotonicity.ACTIVATION, id: '' },
+    { source: 'SBF', target: 'YHP1', observable: true, monotonicity: Monotonicity.ACTIVATION, id: '' },
+    { source: 'SFF', target: 'ACE2', observable: true, monotonicity: Monotonicity.ACTIVATION, id: '' },
+    { source: 'SFF', target: 'SWI5', observable: true, monotonicity: Monotonicity.ACTIVATION, id: '' },
+    { source: 'CLN3', target: 'MBF', observable: true, monotonicity: Monotonicity.ACTIVATION, id: '' },
+    { source: 'MBF', target: 'SBF', observable: true, monotonicity: Monotonicity.ACTIVATION, id: '' },
+    { source: 'YOX1', target: 'SBF', observable: true, monotonicity: Monotonicity.INHIBITION, id: '' },
+    { source: 'YHP1', target: 'SBF', observable: true, monotonicity: Monotonicity.INHIBITION, id: '' },
+    { source: 'CLN3', target: 'SBF', observable: true, monotonicity: Monotonicity.ACTIVATION, id: '' },
+    { source: 'MBF', target: 'HCM1', observable: true, monotonicity: Monotonicity.ACTIVATION, id: '' },
+    { source: 'SBF', target: 'HCM1', observable: true, monotonicity: Monotonicity.ACTIVATION, id: '' },
+    { source: 'SBF', target: 'SFF', observable: true, monotonicity: Monotonicity.ACTIVATION, id: '' },
+    { source: 'HCM1', target: 'SFF', observable: true, monotonicity: Monotonicity.ACTIVATION, id: '' }
+  ]
+
 }
 
 interface IRegulation {
@@ -276,5 +390,18 @@ interface IRegulation {
   target: string
   observable: boolean
   monotonicity: Monotonicity
+}
 
+interface INodeData {
+  id: string
+  name: string
+  position: Position
+}
+
+interface IEdgeData {
+  id: string
+  source: string
+  target: string
+  observable: boolean
+  monotonicity: Monotonicity
 }
