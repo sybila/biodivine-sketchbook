1eb24b1e07fbfbf1397e1000de21b891614bed25
Ondřej Huvar <492849@mail.muni.cz>
2023-11-25 19:40:15 +0100

    Change strategy for generating IDs.

--- src-tauri/src/sketchbook/_impl_regulations_state/_impl_id_generating.rs
+++ src-tauri/src/sketchbook/_impl_regulations_state/_impl_id_generating.rs
@@ -4,14 +4,12 @@ use crate::sketchbook::{LayoutId, RegulationsState, VarId};
 impl RegulationsState {
     /// Generate valid `VarId` that's currently not used by any variable in this `RegulationsState`.
     ///
-    /// First, the variable's name or its transformation are tried. If they are both invalid,
-    /// a numerical identifier is constructed.
+    /// First, the variable's name or its transformation by replacing invalid characters are tried.
+    /// If they are both invalid (non-unique), a numerical identifier is added at the end.
     ///
-    /// **warning** Do not use this to pre-generate more than one id.
+    /// **Warning:** Do not use this to pre-generate more than one id, as it is deterministic.
     pub fn generate_var_id(&self, var_name: &str) -> VarId {
-        // TODO: how to generate the valid VarId properly?
-
-        // first try to generate the id using the name
+        // first try to generate the id using the given name
         if let Ok(var_id) = VarId::new(var_name) {
             // the id must not be valid in the network already (that would mean it is already used)
             if !self.is_valid_var_id(&var_id) {
@@ -28,29 +26,26 @@ impl RegulationsState {
             }
         }
 
-        // finally using the numeric ID, starting at `n_vars`, until we find valid one
-        let n_vars = self.num_vars();
-        for n in 0..n_vars {
-            // valid identifier string
-            let var_id = VarId::new(format!("v_{}", n_vars + n).as_str()).unwrap();
+        // finally, append a number after the name
+        // start searching at 0, until we try `self.num_vars()` options
+        for n in 0..self.num_vars() {
+            let var_id = VarId::new(format!("{}_{}", transformed_name, n).as_str()).unwrap();
             if !self.is_valid_var_id(&var_id) {
                 return var_id;
             }
         }
 
         // this must be valid, we already tried more than self.num_vars() options
-        VarId::new(format!("v_{}", n_vars * 2).as_str()).unwrap()
+        VarId::new(format!("{}_{}", transformed_name, self.num_vars()).as_str()).unwrap()
     }
 
     /// Generate valid `LayoutId` that's currently not used by layouts in this `RegulationsState`.
     ///
-    /// First, the layout's name or its transformation are tried. If they are both invalid,
-    /// a numerical identifier is constructed.
+    /// First, the variable's name or its transformation by replacing invalid characters are tried.
+    /// If they are both invalid (non-unique), a numerical identifier is added at the end.
     ///
-    /// **warning** Do not use this to pre-generate more than one id.
+    /// **Warning:** Do not use this to pre-generate more than one id, as it is deterministic.
     pub fn generate_layout_id(&self, layout_name: &str) -> LayoutId {
-        // TODO: how to generate the valid LayoutId properly?
-
         // first try to generate the id using the name
         if let Ok(layout_id) = LayoutId::new(layout_name) {
             // the id must not be valid in the network already (that would mean it is already used)
@@ -68,18 +63,17 @@ impl RegulationsState {
             }
         }
 
-        // finally using the numeric ID, starting at `n_layouts`, until we find valid one
-        let n_layouts = self.num_layouts();
-        for n in 0..n_layouts {
-            // valid identifier string
-            let layout_id = LayoutId::new(format!("l_{}", n_layouts + n).as_str()).unwrap();
+        // finally, append a number after the name
+        // start searching at 0, until we try `self.num_layouts()` options
+        for n in 0..self.num_layouts() {
+            let layout_id = LayoutId::new(format!("{}_{}", transformed_name, n).as_str()).unwrap();
             if !self.is_valid_layout_id(&layout_id) {
                 return layout_id;
             }
         }
 
         // this must be valid, we already tried more than self.num_layouts() options
-        LayoutId::new(format!("l_{}", n_layouts * 2).as_str()).unwrap()
+        LayoutId::new(format!("{}_{}", transformed_name, self.num_layouts()).as_str()).unwrap()
     }
 
     /// **(internal)** Transform a string to an identifier string by removing all the
@@ -113,38 +107,37 @@ mod tests {
     #[test]
     fn test_var_id_generating() {
         let reg_state =
-            RegulationsState::new_from_vars(vec![("a0", "name"), ("a1", "name"), ("a2", "name")])
+            RegulationsState::new_from_vars(vec![("a", "name"), ("b", "name"), ("c", "name")])
                 .unwrap();
         assert_eq!(reg_state.num_vars(), 3);
 
         // name slice that is a valid identifier as is
-        let var_name_1 = "a3";
+        let var_name_1 = "d";
         assert_eq!(
             reg_state.generate_var_id(var_name_1),
-            VarId::new("a3").unwrap()
+            VarId::new("d").unwrap()
         );
 
         // name that is not a valid identifier as it contains various invalid characters
-        let var_name_2 = "a ??)&4    ";
+        let var_name_2 = "-d ??)&    ";
         assert_eq!(
             reg_state.generate_var_id(var_name_2),
-            VarId::new("a4").unwrap()
+            VarId::new("d").unwrap()
         );
 
         // name that is already used in the network
-        let var_name_3 = "a1";
-        // result will be "v_" + numerical index
-        // search for unused index is incremental and starts at <CURRENT_NUM_OF_VARS>
+        let var_name_3 = "a";
+        // result will contain an numerical index in the end
         assert_eq!(
             reg_state.generate_var_id(var_name_3),
-            VarId::new("v_3").unwrap()
+            VarId::new("a_0").unwrap()
         );
     }
 
     #[test]
     fn test_layout_id_generating() {
         let mut reg_state = RegulationsState::new();
-        let layout_id = LayoutId::new("l_1").unwrap();
+        let layout_id = LayoutId::new("l_0").unwrap();
         let default_layout_id = RegulationsState::get_default_layout_id();
         reg_state
             .add_layout_copy(layout_id, "name", &default_layout_id)
@@ -152,20 +145,27 @@ mod tests {
         assert_eq!(reg_state.num_layouts(), 2);
 
         // expected result for all the following IDs will be the same
-        let expected = LayoutId::new("l_2").unwrap();
+        let expected = LayoutId::new("l_1").unwrap();
 
         // name slice that is a valid identifier as is
-        let name_1 = "l_2";
+        let name_1 = "l_1";
         assert_eq!(reg_state.generate_layout_id(name_1), expected);
 
         // name that is not a valid identifier as it contains various invalid characters
-        let name_2 = "%%%%l_    2)";
+        let name_2 = "%%%%l_    1)";
         assert_eq!(reg_state.generate_layout_id(name_2), expected);
 
-        // name that is already used in the network
-        let name_3 = "l_1";
-        // result will be "l_" + numerical index
-        // search for unused index is incremental and starts at <CURRENT_NUM_OF_LAYOUTS>
+        // add new layout
+        let layout_id = LayoutId::new("l").unwrap();
+        reg_state
+            .add_layout_copy(layout_id, "name", &default_layout_id)
+            .unwrap();
+
+        // try generate ID for the same layout again - the result will have numerical index appended
+        // however, this time we cant just add index 0 because the result would not be unique
+
+        let name_3 = "l";
+        // search for unused index is incremental, starting at 0 (until valid index 1 is found)
         assert_eq!(reg_state.generate_layout_id(name_3), expected);
     }
 }
