4bff010c61b5ac0b1c011ae8c258a01afe58945f
Samuel Pastva <sam.pastva@gmail.com>
2024-05-19 12:52:59 +0200

    Fix `window.outerWidth == 0` bug on macOS and update tauri.

--- src/html/component/regulations-editor/rename-dialog/rename-dialog.ts
+++ src/html/component/regulations-editor/rename-dialog/rename-dialog.ts
@@ -3,6 +3,7 @@ import { customElement, query } from 'lit/decorators.js'
 import style_less from './rename-dialog.less?inline'
 import { emit, type Event as TauriEvent, once } from '@tauri-apps/api/event'
 import { appWindow, LogicalSize } from '@tauri-apps/api/window'
+import { type } from '@tauri-apps/api/os';
 
 @customElement('rename-dialog')
 export class RenameDialog extends LitElement {
@@ -20,8 +21,20 @@ export class RenameDialog extends LitElement {
       if (this.nameField !== undefined) this.nameField.value = this.name
     })
     await emit('loaded', {})
-    this.variableIdField?.focus()
-    await appWindow.setSize(new LogicalSize(window.outerWidth, (document.querySelector('body')?.offsetHeight ?? 200) + 20))
+    this.variableIdField?.focus()    
+    let measured_width = document.querySelector('body')?.offsetWidth ?? 400
+    let measured_height = (document.querySelector('body')?.offsetHeight ?? 200)
+    if (await type() == 'Darwin') {
+      // Currently, it seems that setSize includes the size of the title bar, but there
+      // is no exact way to actually measure the title bar, so we just add 40 to the height.
+      // Furthermore, this seems to be a difference in how window size is treated 
+      // between macOS and Windows/Linux.
+      // 
+      // See also: https://github.com/tauri-apps/tauri/issues/6333
+      measured_height += 40
+    }
+    console.log(measured_width, measured_height)
+    await appWindow.setSize(new LogicalSize(measured_width, measured_height))
   }
 
   private async handleSubmit (event: Event): Promise<void> {
@@ -57,7 +70,7 @@ export class RenameDialog extends LitElement {
     return html`
       <div class="uk-container">
         <form class="uk-form-horizontal">
-          <div class="uk-margin-small">
+          <div class="uk-margin-small uk-margin-small-top">
             <label class="uk-form-label" for="form-horizontal-text">Node ID</label>
             <div class="uk-form-controls">
               <input class="uk-input" @input="${this.handleIdUpdate}" id="node-id" type="text" placeholder="ID" />
