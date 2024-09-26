5a9f3a026c2009a2d818f267a1ec2a5ec79ac09b
Ondřej Huvar <492849@mail.muni.cz>
2024-09-26 22:05:06 +0200

    Modify the way how computation progress/results are displayed.

--- src/html/component-analysis/analysis-component/analysis-component.ts
+++ src/html/component-analysis/analysis-component/analysis-component.ts
@@ -25,10 +25,15 @@ export default class AnalysisComponent extends LitElement {
   @state() results: InferenceResults | StaticCheckResults | DynamicCheckResults | null = null
   // Track the state of the "Randomize" checkbox for sampling
   @state() isRandomizeChecked: boolean = false
-  // ID of the `setInterval` we use for pinging backend to get resultss
+  // ID of the `setInterval` we use for pinging backend to get results
   @state() pingIntervalId: ReturnType<typeof setInterval> | undefined = undefined
-  // Text displayed when waiting for full results (can be updated when getting new progress)
-  @state() computationProgressMessage: string = ''
+  // Number of times backend was pinged already (for current computation)
+  @state() pingCounter: number = 0
+  // Main HTML text displayed when waiting for analysis results (can depend on analysis type
+  // and can be updated during computation)
+  @state() waitingMainMessage: string = ''
+  // Intermediate progress report when waiting for analysis results (can be updated during computation)
+  @state() waitingProgressReport: string = ''
 
   constructor () {
     super()
@@ -104,11 +109,12 @@ export default class AnalysisComponent extends LitElement {
       console.log('Error starting inference analysis.')
     }
 
-    this.computationProgressMessage = 'Initiating inference with all properties. This might take some time.\n'
-    this.computationProgressMessage += '\n--------------\nIntermediate progress:\n--------------\n'
+    this.waitingMainMessage = 'Computation is running. Waiting for the results.<br>'
+    this.waitingProgressReport += 'Intermediate progress report:\n--------------\n'
 
     // start pinging backend
-    this.pingIntervalId = setInterval(function () {
+    this.pingIntervalId = setInterval(() => {
+      this.pingCounter += 1
       aeonState.analysis.pingForInferenceResults()
     }, inferencePingTimer)
   }
@@ -120,11 +126,12 @@ export default class AnalysisComponent extends LitElement {
       console.log('Error starting inference analysis.')
     }
 
-    this.computationProgressMessage = 'Initiating inference with static properties. This might take some time.\n'
-    this.computationProgressMessage += '\n--------------\nIntermediate progress:\n--------------\n'
+    this.waitingMainMessage = 'Computation is running. Waiting for the results.<br>'
+    this.waitingProgressReport += 'Intermediate progress report:\n--------------\n'
 
     // start pinging backend
-    this.pingIntervalId = setInterval(function () {
+    this.pingIntervalId = setInterval(() => {
+      this.pingCounter += 1
       aeonState.analysis.pingForInferenceResults()
     }, inferencePingTimer)
   }
@@ -136,33 +143,37 @@ export default class AnalysisComponent extends LitElement {
       console.log('Error starting inference analysis.')
     }
 
-    this.computationProgressMessage = 'Initiating inference with dynamic properties. This might take some time.\n'
-    this.computationProgressMessage += '\n--------------\nIntermediate progress:\n--------------\n'
+    this.waitingMainMessage = 'Computation is running. Waiting for the results.<br>'
+    this.waitingProgressReport += 'Intermediate progress report:\n--------------\n'
 
     // start pinging backend
-    this.pingIntervalId = setInterval(function () {
+    this.pingIntervalId = setInterval(() => {
+      this.pingCounter += 1
       aeonState.analysis.pingForInferenceResults()
     }, inferencePingTimer)
   }
 
   #onComputationUpdateMessageReceived (message: string): void {
     console.log(message)
-    this.computationProgressMessage += message
+    this.waitingProgressReport += message
   }
 
   #onComputationErrorMessageReceived (message: string): void {
     console.log(message)
-    this.computationProgressMessage = 'Error running inference:\n\n' + message
+    this.waitingMainMessage = 'Inference computation ended with an error.<br>'
+    this.waitingProgressReport = 'Error running inference:\n\n' + message
 
     // stop pinging backend
     clearInterval(this.pingIntervalId)
     this.pingIntervalId = undefined
+    this.pingCounter = 0
   }
 
   #onInferenceResultsReceived (results: InferenceResults): void {
     // stop pinging backend
     clearInterval(this.pingIntervalId)
     this.pingIntervalId = undefined
+    this.pingCounter = 0
 
     this.results = results
     console.log('Received full inference results.')
@@ -172,6 +183,7 @@ export default class AnalysisComponent extends LitElement {
     // stop pinging backend
     clearInterval(this.pingIntervalId)
     this.pingIntervalId = undefined
+    this.pingCounter = 0
 
     this.results = results
     console.log('Received static check results.')
@@ -181,6 +193,7 @@ export default class AnalysisComponent extends LitElement {
     // stop pinging backend
     clearInterval(this.pingIntervalId)
     this.pingIntervalId = undefined
+    this.pingCounter = 0
 
     this.results = results
     console.log('Received dynamic check results.')
@@ -208,11 +221,16 @@ export default class AnalysisComponent extends LitElement {
   }
 
   // Method to format the results for display
-  private formatResults (results: InferenceResults | StaticCheckResults | DynamicCheckResults): string {
-    return 'Analysis finished!\n\n' +
-      `Number of satisfying networks: ${results.num_sat_networks}\n` +
-      `Computation time: ${results.comp_time} seconds\n\n\n` +
-      'Computation metadata:\n' +
+  private formatResultsOverview (results: InferenceResults | StaticCheckResults | DynamicCheckResults): string {
+    const compTime = Math.max(results.comp_time, 1) // just in case, to not have "0 seconds"
+    return 'Analysis finished!<br><br>' +
+      `Number of satisfying networks: ${results.num_sat_networks}<br>` +
+      `Computation time: ${compTime} seconds<br>`
+  }
+
+  // Method to format the results for display
+  private formatResultsMetadata (results: InferenceResults | StaticCheckResults | DynamicCheckResults): string {
+    return 'Computation metadata:\n' +
       '--------------\n' +
       `Analysis type: ${results.analysis_type}\n` +
       `${results.metadata_log}\n`
@@ -224,13 +242,15 @@ export default class AnalysisComponent extends LitElement {
     // stop pinging backend
     clearInterval(this.pingIntervalId)
     this.pingIntervalId = undefined
+    this.pingCounter = 0
 
     // reset event to backend
     aeonState.analysis.resetAnalysis()
 
     // clear analysis settings and results
     this.selected_analysis = null
-    this.computationProgressMessage = ''
+    this.waitingMainMessage = ''
+    this.waitingProgressReport = ''
     this.results = null
   }
 
@@ -283,19 +303,11 @@ export default class AnalysisComponent extends LitElement {
               <h3 class="uk-heading-bullet uk-margin-remove-bottom">Inference</h3>
             </div>
   
-            <!-- Buttons for resetting analysis -->
-            <div class="reset-buttons">
-              <button class="uk-button uk-button-large uk-button-secondary"
-                      @click="${() => {
-                        this.resetAnalysis()
-                      }}">Start new computation
-              </button>
-            </div>
-  
-            <!-- Conditionally render analysis buttons only if no analysis is selected -->
+            <!-- Conditionally render analysis buttons only if no analysis is selected,
+                 Otherwise, render a button for resetting analysis. -->
             ${this.selected_analysis === null
 ? html`
-              <div class="uk-flex uk-flex-row uk-flex-center">
+              <div class="uk-flex uk-flex-row uk-flex-center" style="margin-top: 90px">
                 <button class="uk-button uk-button-large uk-button-secondary"
                         @click="${() => {
                           this.runInference()
@@ -314,22 +326,34 @@ export default class AnalysisComponent extends LitElement {
                 </button>
               </div>
             `
-: ''}
-  
+: html`
+              <div class="reset-buttons">
+                <button class="uk-button uk-button-large uk-button-secondary"
+                        @click="${() => {
+                          this.resetAnalysis()
+                        }}">Start again
+                </button>
+              </div>
+            `
+}
             <!-- Conditionally render results window when analysis starts, centered on screen -->
             ${this.selected_analysis !== null
 ? html`
-              <div class="results-window" style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
-                <textarea rows="12" cols="70" readonly style="text-align: center;">${this.results !== null ? this.formatResults(this.results) : this.computationProgressMessage}</textarea>
+              <div class="results-window">
+                <div class="overview-message"
+                  .innerHTML="${this.results !== null ? this.formatResultsOverview(this.results) : this.waitingMainMessage + '.'.repeat(this.pingCounter % 4 + 1) + '<br>'}">
+                </div>
+
+                <textarea rows="10" cols="70" readonly style="text-align: center;">${this.results !== null ? this.formatResultsMetadata(this.results) : this.waitingProgressReport}</textarea>
   
                 <!-- Conditionally render "Sample network" section if results are set -->
                 ${this.results !== null
 ? html`
-                  <div class="sample-options" style="margin-top: 20px; text-align: center;">
-                    <label>Witness networks:</label>
+                  <div class="sample-options">
+                    <label>Candidate networks sampling:</label>
                     <div style="display: flex; align-items: center; justify-content: center;">
-                      <label>Witness count</label>
-                      <input type="number" min="1" .value="${1}" id="witness-count" style="width: 50px; text-align: center; margin-left: 5px; margin-right: 15px;">
+                      <label>Network count</label>
+                      <input  type="number" min="1" .value="${1}" id="witness-count">
   
                       <label>Randomize</label>
                       <input type="checkbox" id="randomize" .checked="${this.isRandomizeChecked}" @change="${this.handleRandomizeChange}" style="margin-left: 5px;">
@@ -338,14 +362,14 @@ export default class AnalysisComponent extends LitElement {
                       ${this.isRandomizeChecked
 ? html`
                         <label style="margin-left: 15px;">Random seed</label>
-                        <input type="number" id="random-seed" .value="${0}" style="width: 50px; text-align: center; margin-left: 5px;">
+                        <input type="number" id="random-seed" .value="${0}">
                       `
 : ''}
                     </div>
-                    <button class="uk-button uk-button-large uk-button-secondary"
+                    <button id="generate-network-button" class="uk-button uk-button-large uk-button-secondary"
                             @click="${async () => {
                               await this.sampleNetworks()
-                            }}">Generate network(s)
+                            }}">Sample network(s)
                     </button>
                   </div>
                 `
