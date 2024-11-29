9866f68210517dc4d8e7bed713fde4753104fcde
Ondřej Huvar <492849@mail.muni.cz>
2024-11-29 19:05:33 +0100

    Switch to universal satisfiability for HCTL properties.

--- src-tauri/src/algorithms/eval_dynamic/eval.rs
+++ src-tauri/src/algorithms/eval_dynamic/eval.rs
@@ -9,6 +9,19 @@ use biodivine_hctl_model_checker::model_checking::model_check_formula_dirty;
 use biodivine_lib_param_bn::biodivine_std::traits::Set;
 use biodivine_lib_param_bn::symbolic_async_graph::{GraphColors, SymbolicAsyncGraph};
 
+/// Model check a property and get colors for which the property holds universally (in every state).
+fn model_check_colors_universal(
+    stg: &SymbolicAsyncGraph,
+    formula: &str,
+) -> Result<GraphColors, String> {
+    // run model checking to compute all valid stat-color pairs
+    let mc_results = model_check_formula_dirty(formula, stg)?;
+    // do universal projection on the colors of the given `colored_vertices`.
+    let complement = stg.unit_colored_vertices().minus(&mc_results);
+    let universal_colors = stg.unit_colors().minus(&complement.colors());
+    Ok(universal_colors)
+}
+
 /// Evaluate given dynamic property given the symbolic transition graph.
 pub fn eval_dyn_prop(
     dyn_prop: ProcessedDynProp,
@@ -17,8 +30,7 @@ pub fn eval_dyn_prop(
     match &dyn_prop {
         ProcessedDynProp::ProcessedHctlFormula(prop) => {
             // use HCTL model checking
-            let mc_results = model_check_formula_dirty(&prop.formula, graph)?;
-            Ok(mc_results.colors())
+            model_check_colors_universal(graph, &prop.formula)
         }
         ProcessedDynProp::ProcessedAttrCount(prop) => {
             // custom implementation (can be made more efficient if needed)
@@ -40,8 +52,7 @@ pub fn eval_dyn_prop(
             // get colors where all the observations are (general) trap spaces
             let trap_space_formula =
                 encode_dataset_hctl_str(&prop.dataset, None, DataEncodingType::TrapSpace)?;
-            let mc_results = model_check_formula_dirty(&trap_space_formula, graph)?;
-            let mut sat_colors = mc_results.colors();
+            let mut sat_colors = model_check_colors_universal(graph, &trap_space_formula)?;
 
             // TODO: if needed, restrict colors to only a set where the TSs are minimal or non-percolable
             if prop.minimal || prop.nonpercolable {
